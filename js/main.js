8a5b575ef046faf18dbb39bb32ee4be209b67b53
lawrence ardosa <lawrenceardosa@gmail.com>
2020-09-09 07:43:48 +0800

    commited 9/9/20

---
+++ js/main.js
@@ -0,0 +1,156 @@
+// ------------- VARIABLES ------------- //
+var ticking = false;
+var isFirefox = /Firefox/i.test(navigator.userAgent);
+var isIe =
+  /MSIE/i.test(navigator.userAgent) ||
+  /Trident.*rv\:11\./i.test(navigator.userAgent);
+var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
+var slideDurationSetting = 600; //Amount of time for which slide is "locked"
+var currentSlideNumber = 0;
+var totalSlideNumber = $(".background").length;
+
+// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+function parallaxScroll(evt) {
+  if (isFirefox) {
+    //Set delta for Firefox
+    delta = evt.detail * -120;
+  } else if (isIe) {
+    //Set delta for IE
+    delta = -evt.deltaY;
+  } else {
+    //Set delta for all other browsers
+    delta = evt.wheelDelta;
+  }
+
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+function slideDurationTimeout(slideDuration) {
+  setTimeout(function () {
+    ticking = false;
+  }, slideDuration);
+}
+
+// ------------- ADD EVENT LISTENER ------------- //
+var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+// ------------- SLIDE MOTION ------------- //
+function nextItem() {
+  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+}
+
+function previousItem() {
+  var $currentSlide = $(".background").eq(currentSlideNumber);
+  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+}
+
+///////////SWIPER////////
+
+// Params
+var sliderSelector = ".swiper-container",
+  options = {
+    init: false,
+    loop: true,
+
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    speed: 1000,
+    slidesPerView: 2, // or 'auto'
+    // spaceBetween: 10,
+    centeredSlides: true,
+    effect: "coverflow", // 'cube', 'fade', 'coverflow',
+    coverflowEffect: {
+      rotate: 50, // Slide rotate in degrees
+      stretch: 0, // Stretch space between slides (in px)
+      depth: 100, // Depth offset in px (slides translate in Z axis)
+      modifier: 1, // Effect multipler
+      slideShadows: true, // Enables slides shadows
+    },
+    grabCursor: true,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1023: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+    // Events
+    on: {
+      imagesReady: function () {
+        this.el.classList.remove("loading");
+      },
+    },
+  };
+var mySwiper = new Swiper(sliderSelector, options);
+
+// Initialize slider
+mySwiper.init();
+
+//////////////////
+
+const wrapper = document.querySelectorAll(".cardWrap");
+
+wrapper.forEach((element) => {
+  let state = {
+    mouseX: 0,
+    mouseY: 0,
+    height: element.clientHeight,
+    width: element.clientWidth,
+  };
+
+  element.addEventListener("mousemove", (ele) => {
+    const card = element.querySelector(".card");
+    const cardBg = card.querySelector(".cardBg");
+    state.mouseX = ele.pageX - element.offsetLeft - state.width / 3;
+    state.mouseY = ele.pageY - element.offsetTop - state.height / 3;
+
+    // parallax angle in card
+    const angleX = (state.mouseX / state.width) * 30;
+    const angleY = (state.mouseY / state.height) * -30;
+    card.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg) `;
+
+    // parallax position of background in card
+    const posX = (state.mouseX / state.width) * -40;
+    const posY = (state.mouseY / state.height) * -30;
+    cardBg.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
+  });
+
+  element.addEventListener("mouseout", () => {
+    const card = element.querySelector(".card");
+    const cardBg = card.querySelector(".cardBg");
+    card.style.transform = `rotateY(0deg) rotateX(0deg) `;
+    cardBg.style.transform = `translateX(0px) translateY(0px)`;
+  });
+});
